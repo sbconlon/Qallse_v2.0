321644b4378fb010af7973efd7691076f0016949
Sean <conlon@berkeley.edu>
2019-07-25 21:07:40 -0700

    Changed CPU time to Wall time

--- scripts/4-test_doublet_making.py
+++ scripts/4-test_doublet_making.py
@@ -10,7 +10,6 @@ if __name__ == '__main__':
 	
 	#---- Script Constants
 	test_densities = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0]
-	test_densities = [1.0]
 	trials_per_density = 1
 	input_seed = None
 	
