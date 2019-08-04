977b8bd3a74ce4a1590276ffb999b3c800539e39
Sean <conlon@berkeley.edu>
2019-08-03 18:47:39 -0700

    PC Results

---
+++ scripts/job_scripts/0_25_job_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -C haswell
+#SBATCH -q debug
+#SBATCH -J 0_25_Parallel_Test
+#SBATCH --mail-user=conlon@berkeley.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 00:30:00
+
+#run the application:
+srun -n 1 -c 64 4-0_25_test_doublet_making.py
